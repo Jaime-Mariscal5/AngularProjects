a48071718bf2189e736eb0f12434c38ab1026b4e
Jaime Mariscal <Jaime_Mariscal_04@hotmail.com>
2023-12-14 22:45:35 -0600

    terminacion proyecto gifs , (abierto a mejoras)

--- projects/app-hex-aqu-app-gifs/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ projects/app-hex-aqu-app-gifs/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.css']
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  
+  @Input() url!:string;
+  @Input() alt!:string;
+  public hasLoad: boolean = false;
+
+  ngOnInit(): void {
+   if(!this.url) throw new Error('Method not implemented.');
+  }
+
+  onLoad(){
+    console.log('Imagen loaded');
+    setTimeout(() => {
+      
+      this.hasLoad = true;
+    }, 900);
+  }
 
 }
