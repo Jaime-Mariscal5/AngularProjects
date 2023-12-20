9b0861ffd4db50f3bdcf2e083fd695df39542f58
Jaime Mariscal <jaimemariscal@MacBook-Pro-de-Jaime.local>
2023-12-20 16:44:56 -0600

    Testeo Angular material

--- projects/app-country/src/app/app.module.ts
+++ projects/app-country/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
