3071988c3fd539e043922cc043c46b302e9cddcd
Jaime Mariscal <Jaime_Mariscal_04@hotmail.com>
2023-12-19 19:33:01 -0600

    implementacion mat-sidenav

--- projects/app-country/src/app/app.component.ts
+++ projects/app-country/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appCountry';
+  showMenu = false;
 }
--- projects/app-country/src/app/app.module.ts
+++ projects/app-country/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Materiales xd
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
